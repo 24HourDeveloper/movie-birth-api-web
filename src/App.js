b06602c1423948a36d01749c3e54c94b412049b5
unknown <mikeitt009@gmail.com>
2020-01-13 19:19:35 -0500

    implemented datepicker from material-ui. Got fetch to pull data from springboot rest api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+import "./App.css";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 400
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [date, setDate] = useState("2020-01-13");
+
+  const changeDate = e => {
+    setDate(e.target.value);
+  };
+
+  const getDate = async () => {
+    console.log(date);
+    const response = await fetch(
+      `http://localhost:8080/api/movie?birth=${date}`
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField
+        id="date"
+        label="Enter Your Birthday"
+        type="date"
+        defaultValue={date}
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true
+        }}
+        onChange={changeDate}
+      />
+      <br />
+      <Button variant="contained" color="primary" onClick={getDate}>
+        Submit Birthday
+      </Button>
     </div>
   );
 }
